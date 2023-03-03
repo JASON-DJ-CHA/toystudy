2c1860055303f67a43c3f473831814828a33d6be
chadongju <jason.c9340@gmail.com>
2023-03-03 19:03:46 +0900

    회원가입페이지

--- app/src/main/java/com/example/carwash/SignUpActivity.java
+++ app/src/main/java/com/example/carwash/SignUpActivity.java
@@ -3,7 +3,9 @@ package com.example.carwash;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.carwash.databinding.ActivitySignUpBinding;
 
@@ -20,11 +22,39 @@ public class SignUpActivity extends AppCompatActivity {
 
 //      데이터바인딩사용하기 2
         binding = DataBindingUtil.setContentView(this,R.layout.activity_sign_up);
-        binding
+        String email = binding.etEmailAddress.getText().toString();
+        String pw1 = binding.etPassword1.getText().toString();
+        String pw2 = binding.etPassword2.getText().toString();
+        String nickname = binding.etNickName.getText().toString();
+
+        binding.btnCheck.setOnClickListener(this::emailCheck);
+        binding.btnSignUp.setOnClickListener(this::signup);
+        binding.imgX.setOnClickListener(this::backToLogin);
+
+        // textView들 변경관련 기능 추가해주기.
+
+        // 전체 동의 checkBox 기능 넣어주기
 
 
 
+    }
 
+    private void backToLogin(View view) {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
 
+    private void signup(View view) {
+
+        // 이메일 중복확인 확인. if
+        // 비밀번호 일치 확인 if
+        // 닉네임 2글자 이상 확인 if
+        // checkBox 확인 if
+
+        //
+    }
+
+    private void emailCheck(View view) {
+        //email 중복확인 기능 넣어주기.
     }
 }
