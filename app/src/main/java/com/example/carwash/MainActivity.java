66306fe70cca8aee26c385c51d1d3b23f636f21f
chadongju <jason.c9340@gmail.com>
2023-03-03 17:20:08 +0900

    회원가입페이지

--- app/src/main/java/com/example/carwash/MainActivity.java
+++ app/src/main/java/com/example/carwash/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.carwash;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.example.carwash.databinding.ActivitySignUpBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+//    데이터 바인딩 사용하기1
+    private ActivitySignUpBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//      데이터바인딩사용하기 2
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_sign_up);
+
     }
 }
